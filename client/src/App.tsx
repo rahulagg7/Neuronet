cd348b46cec59c3c88e1d4e17b20a9b0f4f07004
Rahul Aggarwal <rahulagg217@gmail.com>
2025-09-18 15:28:50 +0530

    card component and icons

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,19 @@
 import { Button } from "./components/Button"
+import { Card } from "./components/Card"
 import { PlusIcon } from "./icons/PlusIcon"
 import { ShareIcon } from "./icons/ShareIcon"
 
 function App() {
   return (
-      <div>
-        <Button variant = "primary" text = "Add Content"  startIcon = {<PlusIcon />}></Button>
-        <Button variant = "secondary" text = "Share" startIcon={<ShareIcon />}></Button>
-    </div>
+    <div className="p-4">
+        <div className="flex justify-end gap-4">
+          <Button variant = "primary" text = "Add Content"  startIcon = {<PlusIcon />}></Button>
+          <Button variant = "secondary" text = "Share" startIcon={<ShareIcon />}></Button>
+      </div>
+      <div className = "flex gap-4">
+        <Card type = "youtube" link = "https://www.youtube.com/embed/3c4UyGRBnmM?si=Usaqmz2rQCSL54vn" title="First video"/></div>
+        <Card type = "twitter" link = "https://x.com/elonmusk/status/1519480761749016577" title = "First tweet" ></Card>
+      </div>
   )
 }
 
