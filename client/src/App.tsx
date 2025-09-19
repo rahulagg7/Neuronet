6ff35e75a9f6dfe8a00c275c9a572f04b51871f5
Rahul Aggarwal <rahulagg217@gmail.com>
2025-09-19 12:30:21 +0530

    bug fix tweet not visible

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -11,8 +11,9 @@ function App() {
           <Button variant = "secondary" text = "Share" startIcon={<ShareIcon />}></Button>
       </div>
       <div className = "flex gap-4">
-        <Card type = "youtube" link = "https://www.youtube.com/embed/3c4UyGRBnmM?si=Usaqmz2rQCSL54vn" title="First video"/></div>
+        <Card type = "youtube" link = "https://www.youtube.com/embed/3c4UyGRBnmM?si=Usaqmz2rQCSL54vn" title="First video"/>
         <Card type = "twitter" link = "https://x.com/elonmusk/status/1519480761749016577" title = "First tweet" ></Card>
+      </div>
       </div>
   )
 }
